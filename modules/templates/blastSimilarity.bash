a5f17f2c6f2302f79847b7c8436b4cb636ad04d7
rdemko2332 <rdemko2332@gmail.com>
2023-10-23 11:20:19 -0400

    modules/templates/blastSimilarity.bash

--- modules/templates/blastSimilarity.bash
+++ modules/templates/blastSimilarity.bash
@@ -2,11 +2,13 @@
 
 set -euo pipefail
 
-/usr/bin/ncbi-blast-2.13.0+/bin/$blastProgram -db $databaseName -query $subsetFasta -outfmt "6 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore length nident pident positive qframe sstrand gaps qseq" > out.txt
+$blastProgram -db $databaseName \
+	      -query $subsetFasta \
+	      -outfmt "6 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore length nident pident positive qframe sstrand gaps qseq" > out.txt
 
 if [ "$printSimSeqs" = true ]; then
 
-    perl /usr/bin/printSimSeqs.pl \
+    printSimSeqs.pl \
      --result out.txt \
      --output blastSimilarity.out \
      --minLen $lengthCutoff \
@@ -16,7 +18,7 @@ if [ "$printSimSeqs" = true ]; then
     
 else
 
-    perl /usr/bin/blastSimilarity.pl \
+    blastSimilarity.pl \
      --fasta $subsetFasta \
      --result out.txt \
      --output blastSimilarity.out \
